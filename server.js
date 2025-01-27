db9afff5a86b3597c6800ee2e7cf0427e9f4e2b3
MitchellLayzell <95312053+MitchellLayzell@users.noreply.github.com>
2025-01-27 15:39:54 +0000

    skibidi

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "styles")));
+app.use(express.static(path.join(__dirname, "images")));
 
 const rooms = {};
 
